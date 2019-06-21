e679510c390424f4107008527f86e3d47fa7a8dc
Manuel rojas <mrojasr280@gmail.com>
2019-06-21 10:57:21 -0700

    Final cambios

--- src/main.c
+++ src/main.c
@@ -23,6 +23,7 @@ int main()
   fprintf(fp, "Ida: \n\n");
   fprintf(fp, "Piso 1: Hallando la escalera para subir al piso 2.\n\n");
   write_txt(fila_inicio, columna_inicio, piso, 'a', 'S', fp);
+  fclose(fp);
 
   /* PISO 2, BUSCANDO LLAVE */
 
@@ -35,8 +36,11 @@ int main()
   dfs(fila_inicio, columna_inicio, llave);
   add_visited();
   print_piso();
-  fprintf(fp, "\nPiso 2: Hallando la llave del tesoro\n\n");
-  write_txt(fila_inicio, columna_inicio, piso, 'a', 'L', fp);
+  FILE * fp1;
+  fp1 = fopen ("camino/camino.txt","a");
+  fprintf(fp1, "\nPiso 2: Hallando la llave del tesoro\n\n");
+  write_txt(fila_inicio, columna_inicio, piso, 'a', 'L', fp1);
+  fclose(fp1);
 
   /* PISO 2, BUSCANDO ESCALERA PARA SUBIR */
   
@@ -49,8 +53,11 @@ int main()
   dfs(fila_inicio, columna_inicio, escalera_subir);
   add_visited();
   print_piso();
-  fprintf(fp, "\nPiso 2: Hallando la escalera de subida hacia el piso 3.\n\n");
-  write_txt(fila_inicio, columna_inicio, piso, 'a', 'S', fp);
+  FILE * fp2;
+  fp2 = fopen ("camino/camino.txt","a");
+  fprintf(fp2, "\nPiso 2: Hallando la escalera de subida hacia el piso 3.\n\n");
+  write_txt(fila_inicio, columna_inicio, piso, 'a', 'S', fp2);
+  fclose(fp2);
 
 
   /* PISO 3, Buscando el TESORO */
@@ -63,8 +70,11 @@ int main()
   dfs(fila_inicio, columna_inicio, tesoro);
   add_visited();
   print_piso();
-  fprintf(fp, "\nPiso 3: Hallando el cofre del tesoro.\n\n");
-  write_txt(fila_inicio, columna_inicio, piso, 'a', 'T', fp);
+  FILE * fp3;
+  fp3 = fopen ("camino/camino.txt","a");
+  fprintf(fp3, "\nPiso 3: Hallando el cofre del tesoro.\n\n");
+  write_txt(fila_inicio, columna_inicio, piso, 'a', 'T', fp3);
+  fclose(fp3);
 
 
   /* BAJADA */
@@ -79,10 +89,13 @@ int main()
   dfs(fila_inicio, columna_inicio, escalera_bajar);
   add_visited();
   print_piso();
-  fprintf(fp, "-------------\n");
-  fprintf(fp, "Vuelta: \n\n");
-  fprintf(fp, "\nPiso 3: Hallando la escalera de bajada al piso 2.\n\n");
-  write_txt(fila_inicio, columna_inicio, piso, 'a', 'B', fp);
+  FILE * fp4;
+  fp4 = fopen ("camino/camino.txt","a");
+  fprintf(fp4, "-------------\n");
+  fprintf(fp4, "Vuelta: \n\n");
+  fprintf(fp4, "\nPiso 3: Hallando la escalera de bajada al piso 2.\n\n");
+  write_txt(fila_inicio, columna_inicio, piso, 'a', 'B', fp4);
+  fclose(fp4);
 
 
   //Piso 2 al piso 1
@@ -94,8 +107,11 @@ int main()
   dfs(fila_inicio, columna_inicio, escalera_bajar);
   add_visited();
   print_piso();
-  fprintf(fp, "\n\nPiso 2: Hallando la escalera de bajada al piso 1.\n\n");
-  write_txt(fila_inicio, columna_inicio, piso, 'a', 'B', fp);
+  FILE * fp5;
+  fp5 = fopen ("camino/camino.txt","a");
+  fprintf(fp5, "\n\nPiso 2: Hallando la escalera de bajada al piso 1.\n\n");
+  write_txt(fila_inicio, columna_inicio, piso, 'a', 'B', fp5);
+  fclose(fp5);
 
   //Piso 1 a la salida -> Origen, termina
   free_memory();
@@ -106,7 +122,10 @@ int main()
   dfs(fila_inicio, columna_inicio, inicio);
   add_visited();
   print_piso();
-  fprintf(fp, "\n\nPiso 1: Hallando la salida de la torre.\n\n");
-  write_txt(fila_inicio, columna_inicio, piso, 'a', 'I', fp);
-  fprintf(fp, "\n\n Ha salido de la torre con el tesoro!");
+  FILE * fp6;
+  fp6 = fopen ("camino/camino.txt","a");
+  fprintf(fp6, "\n\nPiso 1: Hallando la salida de la torre.\n\n");
+  write_txt(fila_inicio, columna_inicio, piso, 'a', 'I', fp6);
+  fprintf(fp6, "\n\n Ha salido de la torre con el tesoro!");
+  fclose(fp6);
 }
